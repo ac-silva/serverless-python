756c390a3f017a840be8253662d14b89b485687f
Arthur Carvalho <acsilva.5e@gmail.com>
2024-05-30 15:03:37 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+### homework serverless with Python
+
+
+##### Setup
+  ```sh
+    sh ./script.sh
+  ```
+
+  this file setup localstack to run lambdas
+
+  - create sqs queues
+    - em-preparacao-pizzaria
+    - pronto-pizzaria
+
+  - create dynamodb table
+    - pedidos-pizzaria
+
+#### Deploy
+  run: 
+  ```sh
+    npm run deploy
+  ```
+
+
---
+++ handler.py
@@ -0,0 +1,41 @@
+import os
+import json
+import boto3
+import datetime
+from sqsHandler import SqsHandler
+
+def s3Handler(event, context):
+    record = event['Records'][0]
+    file = record['s3']['object']['key']
+
+    sqsPreparacao = SqsHandler(os.environ.get('SQS_PREPARACAO_URL', ''))
+    sqsPronto = SqsHandler(os.environ.get('SQS_PRONTO_URL', ''))
+
+    # file = ${status}/${pedido}-{cliente}"
+    filename_splited = file.split("/")
+    status = filename_splited[0]
+    pedido_and_cliente = filename_splited[1].split("-")
+    pedido = pedido_and_cliente[0]
+    cliente = pedido_and_cliente[1]
+
+    message = str({
+        "pedido": pedido,
+        "datetime": str(datetime.datetime.now().replace(microsecond=0).isoformat()) + "Z",
+        "cliente": cliente,
+        "status": status
+    })
+
+    if (status == "preparacao"):
+        sqsPreparacao.send(message)
+    else:
+        sqsPronto.send(message)
+
+    return event
+
+def sqsHandler(event, context):
+    record = event['Records'][0]
+    message = json.loads(record['body'])
+    table = boto3.resource('dynamodb').Table(os.environ.get('DYNAMODB_TABLE', ''))
+    table.put_item(Item=message)
+
+    return event
